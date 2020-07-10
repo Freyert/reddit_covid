19fa8de33a0ff215e7876f5c1326e633aad78cfa
Fulton Byrne <fulton.byrne@commercetools.de>
2020-07-09 21:15:34 -0400

    pct_change minimalist look

--- src/reddit_covid/graph.py
+++ src/reddit_covid/graph.py
@@ -1,26 +1,39 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 import importlib.resources as pkg_resources
 import io
+import math
+
+
+# data collection didn't start until a few weeks before this.
+firstCleanPeriod = '2020-04-27'
 
 
 def graph(df):
     with pkg_resources.path(__package__, "graphing") as path:
         plt.style.use(path / "style.mplstyle")
 
-    posIncrease = df['positiveIncrease']
-    yticks = [
-        posIncrease.min(),
-        posIncrease.median(),
-        posIncrease.max()
-    ]
+    fig = plt.figure()
 
-    reversed = df[::-1]
-    plt.stackplot(reversed['date'],
-                  reversed['positiveIncrease'], color='#d38fc5')
-    plt.gcf().set_size_inches(4, 3)
+    ax = fig.add_subplot()
+
+    posIncrease = df.resample(
+        'W-MON', on='date').sum().sort_values(by='date')
+    # get left and bottom spines to cross
+    ax.spines['left'].set_position(('axes', 0.04))
+    ax.spines['bottom'].set_position(('axes', 0.03))
+
+    day_fmt = mdates.DateFormatter('%b %d')
+    ax.xaxis.set_major_locator(mdates.DayLocator())
+    ax.xaxis.set_major_formatter(day_fmt)
+
+    print(posIncrease['positiveIncrease'].pct_change())
+    posIncrease['positiveIncrease'].pct_change(
+    )[firstCleanPeriod:].plot(kind="line", ax=ax)
 
     f = io.BytesIO()
     plt.show()
     exit(0)
-    plt.savefig(f, bbox_inches='tight')
+    fig.savefig(f, bbox_inches='tight')
     return f.getvalue()
